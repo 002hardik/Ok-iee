f0eb183eaee7625fde06807105a662078afa7207
002hardik <hardikyadav72@gmail.com>
2022-11-23 21:41:54 +0530

    updated frontend ui

---
+++ src/component/Chat/Chat.js
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from "react";
+import { user } from "../Join/Join";
+import socketIo from "socket.io-client";
+import "./Chat.css";
+import sendLogo from "../../images/send.png";
+import Message from "../Message/Message";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import closeIcon from "../../images/closeIcon.png";
+import video1 from "../../images/video1.mp4";
+
+const ENDPOINT = "https://chat-application-api.onrender.com" ;
+
+const Chat = () => {
+
+  const [data,setData]=useState();
+  console.log(data)
+
+  const [socket, setSocket] = useState(null);
+  const [id, setid] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+    socket && socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
+
+  console.log(messages);
+  useEffect(() => {
+    let socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+    setSocket(socket);
+
+    socket.on("connect", () => {
+      alert("Connected");
+      setid(socket.id);
+    });
+    console.log(socket);
+    socket.emit("joined", { user });
+
+    socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("leave", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket &&
+      socket.on("sendMessage", (data) => {
+        setMessages([...messages, data]);
+        console.log(data.user, data.message, data.id);
+      });
+    return () => {
+      socket && socket.off();
+    };
+  }, [messages]);
+
+  return (
+    <>
+    <div className='main'>
+        <div className="overlay"></div>
+        <video src={video1} autoPlay loop muted />
+        <div className="content">
+            {/* <h1>Welcome</h1>
+            <p>To my site.</p> */}
+            <div className="chatPage">
+      <div className="chatContainer"> 
+        <div className="header">
+          <h2>Ok-iee 🤖</h2>
+          <a href="/">
+            {" "}
+            <img src={closeIcon} alt="Close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              user={item.id === id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
+        <div className="inputBox">
+          <input
+            onKeyPress={(event) => (event.key === "Enter" ? send() : null)}
+            type="text"
+            id="chatInput"
+          />
+          <button onClick={send} className="sendBtn">
+            <img src={sendLogo} alt="Send" />
+          </button>
+        </div>
+      </div>
+    </div>
+        </div>
+    </div>
+    {/* <div className="chatPage">
+      <div className="chatContainer"> 
+        <div className="header">
+          <h2>Ok-iee 🤖</h2>
+          <a href="/">
+            {" "}
+            <img src={closeIcon} alt="Close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              user={item.id === id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
+        <div className="inputBox">
+          <input
+            onKeyPress={(event) => (event.key === "Enter" ? send() : null)}
+            type="text"
+            id="chatInput"
+          />
+          <button onClick={send} className="sendBtn">
+            <img src={sendLogo} alt="Send" />
+          </button>
+        </div>
+      </div>
+    </div> */}
+    </>
+  );
+};
+
+export default Chat;
---
+++ src/component/Join/Join.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import "./Join.css";
+import image2 from "../../images/image2.png";
+import {Link} from "react-router-dom";
+import video2 from "../../images/video2.mp4";
+
+let user;
+
+const sendUser=()=>{
+  user=document.getElementById('joinInput').value;
+  document.getElementById('joinInput').value = "";
+}
+
+const Join = () => {
+  
+  const [name,setname]=useState("")
+ 
+  return (
+    <>
+    <div className='main'>
+        <div className="overlay"></div>
+        <video src={video2} autoPlay loop muted />
+        <div className="content">
+            {/* <h1>Welcome</h1>
+            <p>To my site.</p> */}
+            <div className='JoinPage'>
+      <div className='JoinContainer'>
+      <img src={image2} alt='logo'/>
+        <h1>Ok-iee 🤖</h1>
+        <input onChange={(e) => setname(e.target.value)} placeholder='Enter Your Name' type="text" id="joinInput"/>
+        <Link onClick={(event)=> !name ?event.preventDefault():null} to="/chat"> <button onClick={sendUser} className='joinbtn'>Login In</button> </Link>
+      </div>
+    </div>
+        </div>
+    </div>
+    {/* <div className='JoinPage'>
+      <div className='JoinContainer'>
+      <img src={image2} alt='logo'/>
+        <h1>Ok-iee 🤖</h1>
+        <input onChange={(e) => setname(e.target.value)} placeholder='Enter Your Name' type="text" id="joinInput"/>
+        <Link onClick={(event)=> !name ?event.preventDefault():null} to="/chat"> <button onClick={sendUser} className='joinbtn'>Login In</button> </Link>
+      </div>
+    </div> */}
+    </>
+  )
+}
+
+export default Join
+export {user}
